f1976328ffaf0cc9b910ad51a193e5165cc03683
Carter Kwon <carter.kwon@asu.edu>
2021-07-25 22:49:28 -0700

    Add prod workflow and remove commit_hash var

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,7 +2,3 @@ variable "aws_region" {
   description = "The AWS region to create resources in."
   default     = "us-east-1"
 }
-
-variable "commit_hash" {
-  description = "The commit hash to be used as an identifier."
-}
