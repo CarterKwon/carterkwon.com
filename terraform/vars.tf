dc05ac144ac90a5637bb5ec310b2f42a636451e6
Carter Kwon <carter.kwon@asu.edu>
2021-07-26 21:36:28 -0700

    Add jinja

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "The AWS region to create resources in."
   default     = "us-east-1"
 }
+
+variable "tags" {
+  description = "resource tags"
+  type        = map(string)
+}
