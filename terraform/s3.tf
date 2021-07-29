4a37c6861861a70d7b820278b11067467ee348d4
Carter Kwon <carter.kwon@asu.edu>
2021-07-28 19:51:56 -0700

    Add TF bucket back in

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
-# resource "aws_s3_bucket" "website_bucket" {
-#   bucket = "carterkwon.com-${terraform.workspace}"
-#   acl    = "private"
-#   tags   = var.tags
-# }
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "carterkwon.com-${terraform.workspace}"
+  acl    = "private"
+  tags   = var.tags
+}
