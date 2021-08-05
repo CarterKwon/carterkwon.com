4ebfdb37972dee5f3f21c029cb0f8d6bd9ab1cb5
Carter Kwon <carter.kwon@asu.edu>
2021-08-04 19:40:32 -0700

    Add footer

--- app/src/App.js
+++ app/src/App.js
@@ -9,12 +9,14 @@ import "sass/type.scss";
 
 import Header from "components/Header";
 import About from "components/About";
+import Footer from "components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
+      <Footer />
     </div>
   );
 }
