836e9c58ed79f1f7181edfa81ae6131089094564
Carter Kwon <carter.kwon@asu.edu>
2021-08-18 23:41:16 -0700

    Add access policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,3 +3,28 @@ resource "aws_s3_bucket" "website_bucket" {
   acl    = "private"
   tags   = var.tags
 }
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    sid    = "GetObjects"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.website.iam_arn]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.website_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_access_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
